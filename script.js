5ee248f86b85b9c47fb75855b15bd9d1ebb3885b
Haider Shawl <34603371+HaiderShawl@users.noreply.github.com>
2023-03-26 12:20:26 +0530

    .

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function init() {
   
     // calculate the new position of the ball based on the gyroscope data
     const dx = gamma / 10; // divide by 10 to reduce the sensitivity
-    const dy = -beta / 10;
+    const dy = beta / 10;
     ball.x += dx;
     ball.y += dy;
   
