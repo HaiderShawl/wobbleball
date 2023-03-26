435d72c8e8a3b773637cc7d736b5985c574dd896
Haider Shawl <34603371+HaiderShawl@users.noreply.github.com>
2023-03-26 15:36:41 +0530

    .

--- script.js
+++ script.js
@@ -101,19 +101,3 @@ function init() {
   // start the game loop
   gameLoop();
 }
-
-
-function requestDeviceOrientation () {
-  if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-  DeviceOrientationEvent.requestPermission()
-  .then(permissionState => {
-  if (permissionState === 'granted') {
-  window.addEventListener('deviceorientation', () => {});
-  }
-  })
-  .catch(console.error);
-  } else {
-  // handle regular non iOS 13+ devices
-  console.log ("not iOS");
-  }
-}
